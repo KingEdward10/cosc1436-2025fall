523739fab868f76f8ee1ba70f2126611f2a590b1
Edward Watts <edward.watts@my.tccd.edu>
2025-09-02 21:42:52 -0500

    Chapter2

---
+++ classwork/Chapter2/Chapter2/Chapter2.cpp
@@ -0,0 +1,18 @@
+// Chapter2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+* Edward Watts
+* lab #
+* COSC 1436 Fall 2025
+*/
+
+#include <iostream>
+
+int main()
+{
+    //std::cout << "Hello World\n" << " " << "Your Name" << std::endl;
+
+    std::cout << "you are " << 20 << " years old" << std::endl;
+    std::string name;
+    name = "Your Name";
+    std::cout << "Hello World\n" << " " << "Your Name" << name;
+}
