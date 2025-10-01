6196694e24b9617b43372ead85513b2bff7f1dfe
Edward Watts <edward.watts@my.tccd.edu>
2025-09-30 21:31:54 -0500

    ll

--- classwork/Chapter 4/Chapter 4/Chapter 4.cpp
+++ classwork/Chapter 4/Chapter 4/Chapter 4.cpp
@@ -18,20 +18,77 @@ int main()
     bool isClassic;                 //Required, false
 
 
+    /*std::cout << "Enter Value: ";
+    int number;
+    std::cin >> number;
+
+    std::cout << "prefix (++x) = " << ++number << " , x = " << number << std::endl;
+    std::cout << "prefix (--x) = " << --number << " , x = " << number << std::endl;
+    std::cout << "postfix (x++) = " << number++ << " , x = " << number << std::endl;
+    std::cout << "postfix (x--) = " << number-- << " , x = " << number << std::endl;*/
+    bool done = false;
+    while (!done)
+
+    {
+        std::cout << "Movie Library" << std::endl;
+        std::cout << "______________" << std::endl;
+        std::cout << "A)dd Movie" << std::endl;
+        std::cout << "V)iew Movies" << std::endl;
+        std::cout << "E)dit movies" << std::endl;
+        std::cout << "D)elete Movie" << std::endl;
+        std::cout << "Q)uit" << std::endl;
+
+        char choice;
+        std::cin >> choice;
+
+
+        switch (choice)
+    
+    {
+        case 'A':
+        case 'a': std::cout << "Add not implemented" << std::endl; done = true; break;
+
+        case 'V':
+        case 'v': std::cout << "View not implemented" << std::endl; done = true; break;
+
+        case 'D':
+        case 'd': std::cout << "Delete not implemented" << std::endl; done = true; break;
+
+        case 'E':
+        case 'e': std::cout << "Edit not implemented" << std::endl; done = true; break;
+
+        case 'Q':
+        case 'q': return 0;
+    }
+
+
     std::cout << "Enter movie title: ";
     std::getline(std::cin, title);
 
     bool isEmpty = title == "";
     if (isEmpty)
-     
+
         std::cout << "Title is required" << std::endl;
 
     std::cout << "Enter the run length (in minutes): ";
+    //runLength = -1;
+    //while (runLength < 0)
+
+    //{
+    //    std::cin >> runLength;
+    //    if (runLength < 0)
+
+    //        std::cout << "ERROR: Run Length must be more than 0" << std::endl;
+
+    //};
     std::cin >> runLength;
-    if (runLength < 0)
+    while (runLength < 0)
     {
-        std::cout << "Run Length must be more than 0" << std::endl;
-        runLength = 0;
+        std::cout << "error : run length must be more than 0: ";
+
+        std::cin >> runLength;
+
+
     };
 
     std::cout << " Enter the release year : ";
@@ -46,19 +103,20 @@ int main()
         releaseYear = 1900;
 
     }*/
-     if (releaseYear < 1900 || releaseYear > 2100)
+    if (releaseYear < 1900 || releaseYear > 2100)
 
         std::cout << "Enter the description : ";
-        std::getline(std::cin, description);
+    std::getline(std::cin, description);
 
-        std::cout << "Enter the movie rating (1.0-10.0) : ";
-        std::cin >> userRating;
-        if (userRating < 1.0 || userRating > 10.0)
-        {
-            std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
+    std::cout << "Enter the movie rating (1.0-10.0) : ";
+    std::cin >> userRating;
+    if (userRating < 1.0 || userRating > 10.0)
+    {
+        std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
 
-        } //else if (userRating > 10.0)
+    } //else if (userRating > 10.0)
 
+    {
         std::cout << "Is this a Classic (Y/N) : ";
         std::string input;
         std::cin >> input;
@@ -70,78 +128,112 @@ int main()
             else
                 std::cout << "you must enter either Y or N";
 
+        //switch(E ^char and interagl
+        //needs to be a constant which is why it is limited to char and int
+        // 
 
         // Logical and ---> &&
         //Logical OR ----> ||
         // Logical Not ---> !
         // short-circuit eval **** important ****
-        {
 
-        }
+            /*
+            * switch-statnent   (needs a block)
+            * declaring a variable (needs a block)
+            *
+            *
+            *
+            *
+            *
+            * x=x-1
+            * x=x+1 used alot these two variable are <0>
+            * prefix increment --- takes variable and increases it by one ++x
+            * prefix decrement --- takes variable and decreases it by one --x
+            * ___________________________________________________________________
+            * postfix increment --- x++
+            * postfix decrement --- x--
+            *
+            * cout << x++ << x;
+            *         10     11
+            * cout << ++x << x;
+            *        12     12
+
+
+
+
+
+            */
 
 
 
 
 
 
-
-
-
-
-
-
+    }
 
 
 
 }
 
-void relationalDemo()
-
-{ 
-    std::cout << "Enter Two Values : ";
-
-    int left, right;
-
-    std::cin >> left >> right;
-
-    // relational operators : = > >= < <= == !=
-    // Warning 1 - Equals and Equality are similar and work in the sane situations
-    // Warning 2 - br vrtu vrtu careful of floating point comparison for equality, consdier using >= or <= unless comparing to an integer 
-    // Warning 3 - Strings compare by character value not by locale 
-    bool areEqual = left == right;
-    std::cout << "areEqual = " << std::endl;
-
-    areEqual = left == right;
-
-    std::cout << "areEqual =" << areEqual << std::endl;
-
-    //areEqual = 10 == left;
-
-    std::cout << " > " << (left > right) << std::endl;
-    std::cout << " < " << (left < right) << std::endl;
-    std::cout << " >= " << (left >= right) << std::endl;
-    std::cout << " != " << (left != right) << std::endl;
-    std::cout << " == " << (left == right) << std::endl;
-
-
-    float someValue = (10 / 3.0) * 3.0;
-    float someOtherValue = (10 * 3.0) / 3.0;
-
-    areEqual = someValue == someOtherValue;
-    std::cout << areEqual << std::endl;
-
-    // Example of Waring 3
-
-    std::string name1 = "Bob";
-    std::string name2 = "bob";
-    std::cout << (name1 == name2) << std::endl;
-    //case matters , Comparison works, but is case sensitive.
-    // to compare strings case sensitive use the strcmpi
-
-    areEqual = _strcmpi(name1.c_str(), name2.c_str()) == 0;
 
 
 
 
 
+
+
+
+
+
+
+//
+//void relationalDemo()
+//
+//{ 
+//    std::cout << "Enter Two Values : ";
+//
+//    int left, right;
+//
+//    std::cin >> left >> right;
+//
+//    // relational operators : = > >= < <= == !=
+//    // Warning 1 - Equals and Equality are similar and work in the sane situations
+//    // Warning 2 - br vrtu vrtu careful of floating point comparison for equality, consdier using >= or <= unless comparing to an integer 
+//    // Warning 3 - Strings compare by character value not by locale 
+//    bool areEqual = left == right;
+//    std::cout << "areEqual = " << std::endl;
+//
+//    areEqual = left == right;
+//
+//    std::cout << "areEqual =" << areEqual << std::endl;
+//
+//    //areEqual = 10 == left;
+//
+//    std::cout << " > " << (left > right) << std::endl;
+//    std::cout << " < " << (left < right) << std::endl;
+//    std::cout << " >= " << (left >= right) << std::endl;
+//    std::cout << " != " << (left != right) << std::endl;
+//    std::cout << " == " << (left == right) << std::endl;
+//
+//
+//    float someValue = (10 / 3.0) * 3.0;
+//    float someOtherValue = (10 * 3.0) / 3.0;
+//
+//    areEqual = someValue == someOtherValue;
+//    std::cout << areEqual << std::endl;
+//
+//    // Example of Waring 3
+//
+//    std::string name1 = "Bob";
+//    std::string name2 = "bob";
+//    std::cout << (name1 == name2) << std::endl;
+//    //case matters , Comparison works, but is case sensitive.
+//    // to compare strings case sensitive use the strcmpi
+//
+//    areEqual = _strcmpi(name1.c_str(), name2.c_str()) == 0;
+//
+//
+//
+//
+//
 }
