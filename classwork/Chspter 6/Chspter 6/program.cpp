0b9724c3fd6af8cd8efb4f77652de57950394e1a
Edward Watts <edward.watts@my.tccd.edu>
2025-10-21 21:23:52 -0500

    push

--- classwork/Chspter 6/Chspter 6/program.cpp
+++ classwork/Chspter 6/Chspter 6/program.cpp
@@ -7,6 +7,10 @@
 #include<string> 
 #include <iomanip>
 #include "program.h"
+// Paramter kinds:
+// 1.) Input / Pass by value
+// 2.) Output / Pass by reference
+// 3.) Input/Output / Pass by reference
 
  //Movie details
 struct Movie
@@ -75,13 +79,51 @@ void DisplayWarning(std::string message)
               << std::endl;
           ResetTextColor();
 }
+bool Confirm (std::string message)
+{
+    std::cout << message << " (Y/N): ";
+    std::string input;
+    std::cin >> input;
+    while (true)
+    {
+        if (_strcmpi(input.c_str(), "Y") == 0)
+        {
+            return true;
+        }
+        else if (_strcmpi(input.c_str(), "N") == 0)
+        {
+            return false;
+        }
+        else
+        {
+            DisplayError("You must enter either Y or N");
+            std::cin >> input;
+        }
+    }
+}
 
+int ReadInt(int minimumValue, int maximumValue)
+{
+    do {
+        int value;
+        std::cin >> value;
+        if (value >= minimumValue && value <= maximumValue)
+            return value;
+            
+        DisplayError(" Value is out of range");
+    } while (true);
+}
 /// <summary>
 ///  <summary> view details of a movie.
 /// <remarks> 
 /// </summary>
 void ViewMovie(Movie movie)
 {
+    if (movie.title == "")
+    {
+        DisplayWarning("No movie exist");
+        return;
+    }
     //Movie movie;
     std::cout << std::fixed << std::setprecision(1) << std::endl;
     std::cout << movie.title << " (" << movie.releaseYear << ")" << std::endl;
@@ -92,42 +134,55 @@ void ViewMovie(Movie movie)
         std::cout << movie.description << std::endl;
     std::cout << std::endl; 
 }
+
+std::string ReadString(std::string message, bool isRequired)
+{
+    std::cout << message;
+
+    std::string input;
+    std::getline(std::cin, input);
+
+    while (isRequired && input == "")
+    {
+        DisplayError("Value is required");
+
+        std::getline(std::cin, input);
+    }
+
+    return input;
+}
 /// <summary>
 /// prompt user and add movie details
 /// </summary>
 /// <param name="done"></param>
-void AddMovie()
+Movie AddMovie()
 
 {
-    Movie movie; //= {0};
+    Movie movie;// = {0};
 
-    std::cout << "Enter movie title: ";
-    std::cin.ignore();
-    std::getline(std::cin, movie.title);
+    //Get movie details
+    movie.title = ReadString("Enter movie title: ", true);
+    movie.releaseYear = ReadInt(1900, 2100);
+    std::cout << "Enter the run length (in minutes): ";
+    movie.runLength = ReadInt(0);
 
-
-    while (movie.title == "");
-    {
-
-        DisplayError("Title is required");
-        std::getline(std::cin, movie.title);
-    }
+   
 
     std::cout << "Enter the run length (in minutes): ";
 
 
 
 
-    std::cin >> movie.runLength;
+    
     do
     {
+        std::cin >> movie.runLength;
         if (movie.runLength < 0)
-        {
             DisplayError("Run length must be more than 0");
             
 
-            std::cin >> movie.runLength;
-        }
+            
+        
 
     } while (movie.runLength < 0);
 
@@ -140,9 +195,7 @@ void AddMovie()
 
         std::cin >> movie.releaseYear;
     }
-    std::cout << "Enter the description : ";
-    std::getline(std::cin, movie.description);
-    std::cout << "Genres" << movie.genres << std::endl;
+    movie.description = ReadString("Enter the optional description: ", false);
 
     //Genres
    // bool done = false;
@@ -165,37 +218,36 @@ void AddMovie()
         //++count
     }
 
-    std::cout << "Is this a Classic (Y/N) : ";
-    std::string input;
-    std::cin >> input;
 
+    movie.isClassic = Confirm("Is this a Classic? : ");
 
+    return movie;
+}
 
-    while (true)
-    {
-        if (_strcmpi(input.c_str(), "Y") == 0)
-        {
-            movie.isClassic = true;
-            break;
-        } else if (_strcmpi(input.c_str(), "N") == 0)
-        {
-            movie.isClassic = false;
-            break;
-        } else {
+void DeleteMovie(Movie& movie)
+{
+    if (!Confirm("Are you sure you want to delete " + movie.title + "?"))
+        return;
 
+    //TODO: Delete movie
+    //DisplayWarning("Not implemented yet");
+    movie.title = "";
+}
 
-            DisplayError( "you must enter either Y or N");
-
-            std::cin >> input;
-
-        }
-    }
+void EditMovie(Movie& movie)
+{
+    DisplayWarning("Not implemented yet");
 }
 int main()
 {
 
 
     // Display main menu
+
+
+
+    Movie movie;
+
     bool done = false;
     do
 
@@ -211,7 +263,7 @@ int main()
         char choice;
         std::cin >> choice;
 
-        Movie movie;
+       
 
         switch (choice)
 
